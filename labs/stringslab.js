2ef97bd0bfe4bff1f421ca929a873de96fa2d582
NathanOneill933 <naynay.oneill@gmail.com>
2025-06-02 18:03:43 -0500

    Lesson 3 Lab Exercise

---
+++ labs/stringslab.js
@@ -0,0 +1,8 @@
+let testSentence = " This Statement has empty spaces."
+
+console.log(testSentence.includes('ate'))
+console.log(testSentence.at(4))
+console.log(testSentence.trim())
+console.log(testSentence.toLowerCase())
+
+console.log("Jack and Jill went up the hill to fetch a pail of water.\nJack fell down and broke his crown and Jill came tumbling after.")
