75970d179c0e4c23bf7354c6614cd11721483fc9
NathanOneill933 <naynay.oneill@gmail.com>
2025-06-04 16:09:45 -0500

    Lesson 4 notes and Lab update

---
+++ labs/numberslab.js
@@ -0,0 +1,32 @@
+//Task One
+
+//Asigning Variables
+
+const variableOne = 34_038
+const variablTwo = 45_838.8734
+const variableThree = 0b0110_0010_1011
+const variableFour = 0x174_C0
+const variableFive = -895_612
+
+//Displaying Variables
+
+console.log(variableOne);
+console.log(variablTwo);
+console.log(variableThree);
+console.log(variableFour);
+console.log(variableFive);
+
+//Task Two
+
+//Assigning Variables
+
+const binaryVariable = 0b11101
+const octalVariable = 0o35
+const hexadecimalVariable = 0x1D
+
+//Displaying Variables
+
+console.log(binaryVariable);
+console.log(octalVariable);
+console.log(hexadecimalVariable);
+
---
+++ lessons/AllAboutNumbersPractice.js
@@ -0,0 +1,76 @@
+//Numbers Example
+const value1 = 42; //Whole
+const value2 = 34.4; //Floating
+const value3 = 34.3;
+
+// console.log(value1);
+// console.log(value2);
+// console.log(value3);
+
+// 17 decimal point precision
+let value4 = 3.99999999999999999
+// console.log(value4);
+
+// BigInt Value
+let value5 = 1435512334451n;
+// console.log(value5);
+
+//Number Systems - 26
+
+// const ex_binary = 0b11010;
+// const ex_hexadecimal = 0x1A;
+// const ex_octal = 0o32;
+
+
+//Exponential Literals
+const aa = 0E-3;
+const bb = 0E+5;
+const cc = 4E2;
+const dd = 234e-2;
+const ee = 2e-3;
+const ff = 56E-4;
+
+// console.log(aa);
+// console.log(bb);
+// console.log(cc);
+// console.log(dd);
+// console.log(ee);
+// console.log(ff);
+
+//Numeric Seperators = Underscores for readability
+const decimalSeperator = 9_563.25;
+const binarySeperator = 0b1010_0001;
+const hexadecimalSeperator = 0xa0_b0_c0;
+
+
+// console.log(decimalSeperator);
+// console.log(binarySeperator);
+// console.log(hexadecimalSeperator);
+
+//Static Properties of Number Object
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MIN_VALUE);
+
+
+//Static Methods of Number Object
+// console.log(Number.isNaN(ff));
+// console.log(Number.isInteger(10.99));
+// console.log(Number.parseInt('432'));
+
+const verifiedInt = Number.parseInt('42.99')
+// console.log(verifiedInt);
+
+const verifiedFloat = Number.parseFloat('42.99')
+// console.log(typeof verifiedFloat);
+
+// Inatance Methods can be accessed from a variable
+// console.log(value1.toExponential());
+// console.log(value1.toFixed(2));
+// console.log(value1.toPrecision(15));
+
+
+
+
+
