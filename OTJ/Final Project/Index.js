c450d45c993f4b7a48f79010dcf605728afd7bd5
WildCreativeKatZ <bri.c.oneill@gmail.com>
2025-06-19 14:27:50 -0500

    index update 1.1

--- OTJ/Final Project/Index.js
+++ OTJ/Final Project/Index.js
@@ -8,16 +8,16 @@ const {
   chooseYourCone,
   getTotalCost
 } = require("./helpers.js");
-const Fruitflavors = require("./fruit_flavors.js");
-const Savoryflavors = require("./savory_flavor.js");
-const Etcflavors = require("./chocoloate_and_other_flavors.js");
+const flavorFruits = require("./fruit_flavors.js");
+const flavorSavory = require("./savory_flavor.js");
+const flavorChocolate = require("./chocoloate_and_other_flavors.js");
 const coneTypes = require("./cone_types.js"); 
 
 const shopTitle =
     "*********************************" +
     "*\n||\t\t\t\t\t\t\t\t||\n||\t  O'NEILL'S ICECREAM SHOP\t||\n||\t\t\t\t\t\t\t\t||\n*" +
     "*********************************";
-    
+
 const welcomeStr = "Welcome to O'neill's Icecream Shop!\n";
 
 console.log(shopTitle);
@@ -31,11 +31,11 @@ if (userInput.toLowerCase() == "n"){
     console.log("Thank you for coming, bye!");
 }else {
     const icecreamflavors = chooseYourflavors();
-    const fruitflavors = chooseYourFruitflavors();
-    const savoryflavors = chooseYourSavoryflavors();
-    const otherflavors = chooseYourEtcflavors();
+    const flavorFruits = chooseYourFruitflavors();
+    const flavorSavory = chooseYourSavoryflavors();
+    const flavorChocolate = chooseYourEtcflavors();
     const cone = chooseYourCone();
-    const orderedIcecream = new icecreamflavors(fruitflavors, savoryflavors, otherflavors, cone)
+    const orderedIcecream = new icecreamflavors(flavorFruits, flavorSavory, flavorChocolate, cone)
 
 
 console.log("Putting together your icecream! Please wait...");
