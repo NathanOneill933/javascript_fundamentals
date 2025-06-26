0ec82f3b1c578338aede339c316009e3efc35262
NathanOneill933 <naynay.oneill@gmail.com>
2025-06-26 11:19:35 -0500

    FP Update - Notes and removing Excess Variables

--- finalProject/Index.js
+++ finalProject/Index.js
@@ -9,11 +9,8 @@ const {
   getTotalCost
 } = require("./helpers.js");
 const iceCream = require("./IceCreamClass.js");
-const flavorFruits = require(".fruit.js");
-const flavorSavory = require(".savory.js");
-const flavorChocolate = require(".chocolate.js");
-const coneTypes = require(".cones.js"); 
 
+//Define Variables for program
 const shopTitle =
     "*********************************" +
     "*\n||\t\t\t\t\t\t\t\t||\n||\t       ICECREAM SHOP \t\t||\n||\t\t\t\t\t\t\t\t||\n*" +
@@ -21,26 +18,40 @@ const shopTitle =
 
 const welcomeStr = "Welcome to our Icecream Shop!\n";
 
+//Print Title and Welcome string
+
 console.log(shopTitle);
 console.log(welcomeStr);
 
+//Promt user for input
 let userInput = prompt("Would you like to order Y or N?");
+
+//Checks if input is invalid, and keeps asking if it is
 while(!checkYorN(userInput)){
     userInput = prompt('please enter letter "Y" or letter "N":');
 }
+
+//If user inputs n, exits out the program
 if (userInput.toLowerCase() == "n"){
     console.log("Thank you for coming, bye!");
 }else {
+    
+    //Gets data to make IceCream
     const icecreamflavors = chooseYourflavors();
     const flavorFruits = chooseYourFruitflavors();
     const flavorSavory = chooseYourSavoryflavors();
     const flavorChocolate = chooseYourEtcflavors();
     const coneTypes = chooseYourCone();
+   
+   //Create a new Icecream with the Users choces
     const orderedIcecream = new icecreamflavors(flavorFruits, flavorSavory, flavorChocolate, coneTypes)
+
+
 
 
 console.log("Putting together your icecream! Please wait...");
 
+//Simulated Delay
 setTimeout(() => {
     console.log("Your icecream is ready!");
     orderedIcecream.showInfo();
