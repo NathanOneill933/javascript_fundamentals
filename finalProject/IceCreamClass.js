70f1cf7a628f8a9595e969cfd0d180875ea4b4da
NathanOneill933 <naynay.oneill@gmail.com>
2025-07-10 09:32:48 -0500

    FP - Index Update

--- finalProject/IceCreamClass.js
+++ finalProject/IceCreamClass.js
@@ -2,81 +2,37 @@
 class Icecream {
   // Private fields to store the properties of the Icecream
   #cone;
-  #flavorChocolate;
-  #flavorSavory;
-  #flavorFruits;
+  #flavor;
 
 
-  // Constructor to initialize the Icecream with flavors and cones
-  constructor(cone, flavorChocolate, flavorFruit, flavorSavory) {
+  // WE NEED TO DEFINE THE VARIABLES 
+  // Constructor to initialize the Icecream with flavors and
+  constructor(cone, flavor) {
     this.#cone = cone;
-    this.#flavorChocolate = flavorChocolate;
-    this.#flavorSavory = flavorSavory;
-    this.#flavorFruits = flavorFruit;
+    this.#flavor = flavor;
   }
 
   // Getters to access the private fields
   get cone() {
     return this.#cone;
   }
-  get flavorChocolate() {
-    return this.#flavorChocolate;
+  get flavor() {
+    return this.#flavor;
   }
-  get flavorSavory() {
-    return this.#flavorSavory;
-  }
-  get flavorFruit() {
-    return this.#flavorFruits;
-  }
-  
+ 
 
   // Method to display the Icecream information
   showInfo() {
     const cone = this.#cone[0];
-    let flavors = "";
+    const flavor = this.#flavor[0];
 
-    // Check if the flavor are an array of objects or a simple array
-    if (this.#flavorChocolate.length == 2 && this.#flavorChocolate[0] != "object") {
-      chocolateflavors = this.#flavorChocolate[0];
-    } else {
-      const arr = [];
+   console.log(
+      `\nYour Icecream is \n Flavor: ${flavor}\n Cone: ${cone}.`
+   );
+   
+    
+  }}
 
-// Loop through the savory flavors array and format each flavor for display
-      for (index = 0; index < this.#flavorChocolate.length; index++) {
-        arr.push(this.#flavorChocolate[index][0]);
-      }
-      flavors = arr.join(", ");
-    }
-    if (this.#flavorFruits.length == 2 && this.#flavorFruits[0] != "object") {
-      fruitflavors = this.#flavorFruits[0];
-    } else {
-      const arr = [];
-
-// Loop through the savory flavors array and format each flavor for display     
-     for (index = 0; index < this.flavor.length; index++) {
-        arr.push(this.#flavorFruits[index][0]);
-      }
-      flavors = arr.join(", ");
-    }
-    if (this.#flavorSavory.length == 2 && this.#flavorSavory[0] != "object") {
-      savoryflavors = this.#flavorSavory[0];
-    } else {
-      const arr = [];
-
-      // Loop through the savory flavors array and format each flavor for display
-      for (index = 0; index < this.flavor.length; index++) {
-        arr.push(this.#flavorSavory[index][0]);
-      }
-      flavors = arr.join(", ");
-    }
-
-
-    // Log the Icecream details to the console
-    console.log(
-      `\nYour Icecream:\nCone - ${cone}\nFlavors - ${flavors}`
-    );
-  }
-}
 
 // Export the Icecream class so it can be used in other files
 module.exports = Icecream;
--- finalProject/Index.js
+++ finalProject/Index.js
@@ -10,11 +10,11 @@ const {
   fruitFlavor,
 } = require("./helpers.js");
 const iceCream = require("./IceCreamClass.js");
-let flavorValue = 0;
 //Define Variables for program
+let flavorValue = 0;
 const shopTitle =
     "*********************************" +
-    "*\n||\t\t\t\t||\n||\t    ICECREAM SHOP \t||\n||\t\t\t\t||\n*" +
+    "*\n||\t\t\t\t||\n||\tTEAM 2 ICECREAM SHOP \t||\n||\t\t\t\t||\n*" +
     "*********************************";
 
 const welcomeStr = "Welcome to our Icecream Shop!\n";
@@ -37,32 +37,33 @@ if (userInput.toLowerCase() == "n"){
     console.log("Thank you for coming, bye!");
     return;
 }else {
-    
-}
     const flavorChoice = ChooseFlavorType();
+    let flavor = "";
     
     if(flavorChoice == "1"){
-        const flavorFruit = fruitFlavor();
+        flavor = fruitFlavor();
+        console.log(flavorFruit)
         flavorValue = 1
     }else if(flavorChoice == "2"){
-         const flavorSavory = chooseSavoryFlavor();
+        flavor = chooseSavoryFlavor();
          flavorValue = 2
     }else if(flavorChoice =="3"){
-        const flavorChocolate = chooseYourEtcflavors();
+        flavor = chooseYourEtcflavors();
         flavorValue = 3
     }
-    
-    const coneTypes = chooseYourCone();
+const coneTypes = chooseYourCone();
    
    //Create a new Icecream with the Users choces
-    // const orderedIcecream = new iceCream(coneTypes, flavorFruit, flavorSavory)
-if(flavorValue == 1){
-    const orderedIcecream = new iceCream(coneTypes, flavorFruit)
-}else if(flavorValue == 2){
-    const orderedIcecream = new iceCream(coneTypes,flavorSavory)
-}else{
-    const orderedIcecream = new iceCream(coneTypes, flavorChocolate)
-}
+    let orderedIcecream = ""
+    orderedIcecream = new iceCream(coneTypes, flavor)
+
+    // if(flavorValue == 1){
+//     orderedIcecream = new iceCream(coneTypes, flavorFruit)
+// }else if(flavorValue == 2){
+//     orderedIcecream = new iceCream(coneTypes,flavorSavory)
+// }else{
+//     orderedIcecream = new iceCream(coneTypes, flavorChocolate)
+// }
 
 
 console.log("Putting together your icecream! Please wait...");
@@ -73,3 +74,8 @@ setTimeout(() => {
     orderedIcecream.showInfo();
     console.log(`\nTotal cost: $${getTotalCost(orderedIcecream)}`);
 }, 3000);
+
+}
+    
+    
+    
